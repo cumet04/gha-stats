a0e58eb501510416a62f2d1fba1ee43c8f6495ce
inomoto <medalhkr@gmail.com>
2023-06-17 23:01:36 +0900

    とりあえず動く

---
+++ actionsService.ts
@@ -0,0 +1,46 @@
+import { Octokit } from "npm:@octokit/rest@19.0.7";
+
+type ActionsClient = InstanceType<typeof Octokit>["rest"]["actions"]
+
+type WorkflowRun = Awaited<
+  ReturnType<ActionsClient["listWorkflowRuns"]>
+>["data"]["workflow_runs"][0];
+
+export class ActionsService {
+  private octokit: Octokit;
+  constructor(githubToken: string, private owner: string, private repo: string) {
+    this.octokit = new Octokit({ auth: githubToken })
+  }
+
+  async workflows() {
+    const resp = await this.octokit.rest.actions.listRepoWorkflows({
+      owner: this.owner,
+      repo: this.repo,
+    })
+    return resp.data.workflows
+  }
+
+  async workflowRunsFor(workflow_id: string | number) {
+    const runs = (await this.octokit.rest.actions.listWorkflowRuns({
+      owner: this.owner,
+      repo: this.repo,
+      workflow_id,
+      per_page: 100, // TODO: いい感じの値にする？
+    })).data.workflow_runs;
+
+    return await Promise.all(runs.map((run) => this.attachJobsTo(run)))
+  }
+
+  private async attachJobsTo(run: WorkflowRun) {
+    const jobs = (await this.octokit.rest.actions.listJobsForWorkflowRun({
+      owner: this.owner,
+      repo: this.repo,
+      run_id: run.id,
+    })).data.jobs;
+
+    return {
+      ...run,
+      jobs,
+    }
+  }
+}
---
+++ main.ts
@@ -0,0 +1,48 @@
+import {
+  difference,
+  parse,
+} from "https://deno.land/std@0.182.0/datetime/mod.ts";
+import { ActionsService } from "./actionsService.ts";
+
+// deno run --allow-read --allow-env --allow-net ./main.ts owner repo
+// tokenに必要な権限の精査はできてない。classicのrepo fullで動くのは確認済
+
+const token = Deno.env.get("GHA_STATS_TOKEN") || Deno.env.get("GITHUB_TOKEN")
+if (!token) throw "token not found"
+const owner = Deno.args[0]
+const repo = Deno.args[1]
+
+const service = new ActionsService(token, owner, repo)
+
+const workflows = await service.workflows()
+console.log(workflows)
+
+const workflow_id = 12345 // console.logを見ながらそれっぽいやつを埋める
+const runs = await service.workflowRunsFor(workflow_id)
+
+const all_steps = runs.flatMap(run =>
+  run.jobs.flatMap(job =>
+    (job.steps ?? []).map(step => {
+      const iso8601 = "yyyy-MM-ddTHH:mm:ss.SSS+09:00";
+      const start = parse(step.started_at!, iso8601);
+      const end = parse(step.completed_at!, iso8601);
+
+      return {
+        job_id: job.id,
+        run_id: job.run_id,
+        workflow_name: run.name ?? "",
+        ...step,
+        seconds: difference(start, end).seconds ?? 0,
+      }
+    })
+  )
+)
+
+const keys = Object.keys(all_steps[0]);
+console.log(keys.join(","));
+
+all_steps.forEach((step) => {
+  // deno-lint-ignore no-explicit-any
+  const values = keys.map((k) => (step as any)[k]);
+  console.log(values.join(",")); // workflow名にカンマがあったら知らん
+});
